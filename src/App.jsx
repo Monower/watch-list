f46e4c0301abf60751d6a7b95eaa5e24afceb460
monower <sadafahmed720@gmail.com>
2023-07-29 23:28:59 +0600

    working on the logic

--- src/App.jsx
+++ src/App.jsx
@@ -50,18 +50,6 @@ export default function App() {
   };
 
   return <>
-    {/* <h3 className="text-[48px] font-bold text-center font-mono underline">Watch List</h3> */}
-{/*     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='test' element={<Test />} />
-          <Route path='*' element={<Error />} />
-        </Route>
-      </Routes>
-    </BrowserRouter> */}
-
-
     <section className="container mx-auto px-4 lg:px-8">
       <div className="flex justify-center items-center h-24 lg:h-52">
         <h3 className="text-[1.5em] lg:text-[4em] font-bold">Welcome to watch-list.</h3>
--- src/Components/App/Input.jsx
+++ src/Components/App/Input.jsx
@@ -1,17 +1,40 @@
 import Button from "./WatchListButton";
+import { useRef } from "react";
+
 export default function Input() {
-    const button_prop = {
-        icon: 'fa-solid fa-plus',
-        color: '#1d4ed8',
-        background: 'slate-500'
+    const input = useRef(null);
+    let name = [];
+    let item = {
+        title: null
     };
 
+    function HandleSubmit(e) {
+        e.preventDefault();
+        // console.log(input.current.value);
+        if (input.current.value === '') {
+            // to clear the storage
+            // localStorage.clear();
+
+            return alert('Input feild cant be empty.');
+        } else {
+            name = JSON.parse(localStorage.getItem('watchList'))
+            name.push({ title: input.current.value });
+            window.localStorage.setItem('watchList', JSON.stringify(name));
+            input.current.value = '';
+            let item = JSON.parse(localStorage.getItem('watchList'))
+            // console.log(item);
+        }
+        
+
+    }
 
 
     return <>
         <div className="flex justify-center items-center">
-            <input className="border border-slate-950 h-10 px-2 focus:outline-none" type="text" placeholder="Enter name of the movie/series..." autoFocus />
-            <button className="bg-slate-950 text-slate-50 h-10 w-10"><i className="fa-solid fa-plus"></i></button>
+            <form onSubmit={(e) => { HandleSubmit(e) }}>
+                <input ref={input} className="border border-slate-950 h-10 px-2 focus:outline-none" type="text" placeholder="Enter name of the movie/series..." autoFocus />
+                <button type="submit" className="bg-slate-950 hover:bg-blue-500 text-slate-50 h-10 w-10"><i className="fa-solid fa-plus"></i></button>
+            </form>
         </div>
     </>;
 }
